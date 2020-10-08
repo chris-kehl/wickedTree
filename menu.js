9efb7820a2cca81e28928e4960e34ffec5cbbb54
chris.kehl <chris_kehl@att.net>
2020-10-08 06:41:58 -0400

    added order template

--- menu.js
+++ menu.js
@@ -25,68 +25,67 @@
 
 //     console.log( total / scores.length);
 
-    const menu = ['pizza', 'cookies', 'eggs', 'apples', 'milk',
-                   'cheeze', 'bread', 'lettuce', 'carrots', 'broccoli',
-                   'potatoes', 'crackers', 'tofu', 'limes', 'cucumbers'];
-    const search = prompt('Search for a product.');
-    let message; 
+    // const order = ['pizza', 'cookies', 'eggs', 'apples', 'milk',
+    //                'cheeze', 'bread', 'lettuce', 'carrots', 'broccoli',
+    //                'potatoes', 'crackers', 'tofu', 'limes', 'cucumbers'];
+    // const search = prompt('Search for a product.');
+    // let message; 
 
-    if (!search) {
-        message = `<strong> Our menu consists of </strong> ${menu.join(', ')}.`;
+    // if (!search) {
+    //     message = `<strong> Our menu consists of </strong> ${order.join(', ')}.`;
 
-    }
-    else if (menu.includes(search.toLowerCase())) {
-        message = `Yes, we have <strong> ${search} </strong>. it's # ${menu.indexOf(search.toLowerCase()) + 1} on the list.`;
-    }   else {
-        message = `Sorry, we do not have <strong> ${search} </strong>.`;
-    }
-    document.querySelector('main').innerHTML = `<p>${message}</p>`;
-
-//      console.log(order1.length)
-//      console.log(order1[0])
-//      console.log(order1[1])
-//      console.log(order1[2])
-
-//      order1.push('Diet coke', 'Small Pizza');
-//      console.log(order1.length)
-
-//      order1.unshift('Sweet tea', `Dressed Cheese Burger`)
-//      console.log(order1.length)
-
-//      order1.pop();
-//      console.log(order1.length)
-
-//      let lastItem = order1.pop();
-//      console.log(lastItem)
-//      console.log(order1.length)
-
-//      let secondItem = order1.shift();
-//      console.log(secondItem)
-//      console.log(order1.length)
-
-// var numbers = [1, 2, 3, 4, 5, 6 ,7 ,8 ,9, 10];
-//      console.log(numbers)
+    // }
+    // else if (order.includes(search.toLowerCase())) {
+    //     message = `Yes, we have <strong> ${search} </strong>. it's # ${menu.indexOf(search.toLowerCase()) + 1} on the list.`;
+    // }   else {
+    //     message = `Sorry, we do not have <strong> ${search} </strong>.`;
+    // }
+    // document.querySelector('main').innerHTML = `<p>${message}</p>`;
 
 
-//      // Spread operator combine arrays, or lists
+ // 1. Create a Multidemensional array ot hold quiz questions
 
-//      const mathStudents = [
-//          'Marty',
-//          'Jennifer',
-//          'Lorraine',
-//          'Goldie'
-//      ];
+ const questions = [
+     ['How many planets are in the Solar Syatem?', '8'],
+     ['How many continents are there?', '7'],
+     ['How many legs does an insect have?', '6'],
+     ['What year was JavaScript created', '1995']
+ ];
 
-//      const scienceStudents = [
-//          'Emmett',
-//          'Clara',
-//          'Needles',
-//          'Strickland'
-//      ];
+ // 2. Store the number of questions answered correctly
+ const correct = [];
+ const incorrect = [];
+ let correctAnswers = 0;
 
-//      const students= [...mathStudents, ...scienceStudents];
-//      console.log(students)
+ // 3. use a loop to cycle through
+
+ for ( let i = 0; i < questions.length; i++ ) {
+     let question = questions[i][0];
+     let answer = questions[i][1];
+     let response = prompt(question);
+
+     if ( response === answer ) {
+         correctAnswers++;
+         correct.push(question);
+     } else {
+         incorrect.push(question);
+     }
+ }
+
+ function createListItems(arr) {
+     let items = '';
+     for (let i = 0; i < arr.length; i++) {
+         items += `<li>${arr[i]}</li>`;
+     }
+     return items;
+ }
 
 
-// var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-// console.log(Math.min(...numbers));
+ let html = `
+<h1>You Got ${correctAnswers} Question(s) Correct</h1>
+<h2> You got these questions right:</h2>
+<ol>${createListItems(correct)}</ol>
+<h2>You got these questions wrong:</h2>
+<ol>${createListItems(incorrect)}</ol>`;
+
+document.querySelector('main').innerHTML = html;
